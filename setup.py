90c4e7c5a404a6da4f45b90dd7f7b1f6877e3b24
Pasha <kletskov@gmail.com>
2024-12-29 00:38:55 -0800

    Refactored structure: renamed package, moved files, updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hockey-blast-common-lib',  # The name of your package
-    version='0.1.0',  # Initial version
+    version='0.1.3',
     description='Common library for shared functionality and DB models',
     author='Pavel Kletskov',
     author_email='kletskov@gmail.com',
