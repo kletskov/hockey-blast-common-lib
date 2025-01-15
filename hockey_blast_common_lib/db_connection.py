f92177ee4deeb818985a211bced3735da240fdae
Pasha <kletskov@gmail.com>
2025-01-14 22:40:36 -0800

    env vars changed a bit, added .env reading , version up

--- hockey_blast_common_lib/db_connection.py
+++ hockey_blast_common_lib/db_connection.py
@@ -1,6 +1,10 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+# Load environment variables from .env file in the root directory of THE PROJECT (not this library)
+load_dotenv()
 
 # Database connection parameters per organization
 DB_PARAMS = {
@@ -14,20 +18,20 @@ DB_PARAMS = {
 
     "frontend-sample-db": {
         "dbname": os.getenv("DB_NAME_SAMPLE", "hockey_blast_sample"),
-        "user": os.getenv("DB_USER_SAMPLE", "frontend_user"),
-        "password": os.getenv("DB_PASSWORD_SAMPLE", "hockey-blast"),
-        "host": os.getenv("DB_HOST_SAMPLE", "localhost"),
-        "port": int(os.getenv("DB_PORT_SAMPLE", 5432))
+        "user": os.getenv("DB_USER", "frontend_user"),
+        "password": os.getenv("DB_PASSWORD", "hockey-blast"),
+        "host": os.getenv("DB_HOST", "localhost"),
+        "port": int(os.getenv("DB_PORT", 5432))
     },
 
-# TODO - the section below is just to handle recovery of sample DB where boss user is present
-# Maybe figure out a way to do backup without it and make frontend_user own the sample? 
+# The section below is to handle recovery of sample DB where boss user is present, to avoid warnings and errors
+# TODO: Maybe figure out a way to do backup without it and make frontend_user own the sample? 
     "boss": {
-        "dbname": os.getenv("DB_NAME_BOSS", "hockey_blast"),
+        "dbname": os.getenv("DB_NAME", "hockey_blast"),
         "user": os.getenv("DB_USER_BOSS", "boss"),
         "password": os.getenv("DB_PASSWORD_BOSS", "boss"),
-        "host": os.getenv("DB_HOST_BOSS", "localhost"),
-        "port": int(os.getenv("DB_PORT_BOSS", 5432))
+        "host": os.getenv("DB_HOST", "localhost"),
+        "port": int(os.getenv("DB_PORT", 5432))
     },
 }
 
